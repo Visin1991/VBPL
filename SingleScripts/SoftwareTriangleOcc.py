e842b816335ade1873f6da8e5ff1be55ad4af36d
Visin <zhuzhanhao1991@gmail.com>
2023-11-03 02:07:18 +0800

    模型可见性剔除

--- SingleScripts/SoftwareTriangleOcc.py
+++ SingleScripts/SoftwareTriangleOcc.py
@@ -4,6 +4,38 @@ import math
 from mathutils.bvhtree import BVHTree
 from mathutils import Vector
 
+# 绘制Debug射线 ......Todo: 后续可以奖这些Debug射线转为Python GPU 进行绘制
+def visualize_rays(camera, resolution=10, ray_length=1000):
+    # Create a new mesh object to visualize the rays
+    mesh = bpy.data.meshes.new(name="Rays")
+    obj = bpy.data.objects.new("Rays", mesh)
+    bpy.context.collection.objects.link(obj)
+
+    verts = []
+    edges = []
+
+    # Iterate over a grid of rays from the camera
+    for x in range(resolution):
+        for y in range(resolution):
+            # Convert grid coordinate to -1..1 range
+            px = x / (resolution - 1) * 2 - 1
+            py = y / (resolution - 1) * 2 - 1
+            
+            # Create start and end points for ray in camera space
+            start = Vector((px, py, -1))
+            end = Vector((px, py, 1))
+            
+            # Convert start and end points to world space
+            start_world = camera.matrix_world @ start
+            end_world = camera.matrix_world @ start - ray_length * (camera.matrix_world @ end - camera.matrix_world @ start).normalized()
+
+            # Append the vertices and edge
+            verts.append(start_world)
+            verts.append(end_world)
+            edges.append([len(verts)-2, len(verts)-1])
+
+    mesh.from_pydata(verts, edges, [])
+
 def cull_hidden_faces_by_camera(obj, camera, resolution=100):
     # Setup BVH tree from object
     bm = bmesh.new()
@@ -28,7 +60,7 @@ def cull_hidden_faces_by_camera(obj, camera, resolution=100):
             
             # Convert start and end points to world space
             start_world = camera.matrix_world @ start
-            end_world = camera.matrix_world @ end
+            end_world = camera.matrix_world @ start - (camera.matrix_world @ end - camera.matrix_world @ start).normalized()
             
             direction = (end_world - start_world).normalized()
 
@@ -41,53 +73,20 @@ def cull_hidden_faces_by_camera(obj, camera, resolution=100):
                 print("miss......")
 
     # Select all faces except the visible ones
-    # for face in bm.faces:
-    #     face.select = face.index not in visible_faces
+    for face in bm.faces:
+        face.select = face.index not in visible_faces
 
     # Delete hidden faces
-    # bmesh.ops.delete(bm, geom=[f for f in bm.faces if f.select], context='FACES')
-    # bm.to_mesh(obj.data)
-    # bm.free()
+    bmesh.ops.delete(bm, geom=[f for f in bm.faces if f.select], context='FACES')
+    bm.to_mesh(obj.data)
+    bm.free()
     for face in visible_faces:
         print(face)
 
 # Assuming the active object is your mesh and you have a camera named "Camera"
-# obj = bpy.context.active_object
-# camera = bpy.data.objects["Camera"]
-# cull_hidden_faces_by_camera(obj, camera)
-
-
-# 绘制Debug射线 ......Todo: 后续可以奖这些Debug射线转为Python GPU 进行绘制
-def visualize_rays(camera, resolution=100, ray_length=10):
-    # Create a new mesh object to visualize the rays
-    mesh = bpy.data.meshes.new(name="Rays")
-    obj = bpy.data.objects.new("Rays", mesh)
-    bpy.context.collection.objects.link(obj)
-
-    verts = []
-    edges = []
-
-    # Iterate over a grid of rays from the camera
-    for x in range(resolution):
-        for y in range(resolution):
-            # Convert grid coordinate to -1..1 range
-            px = x / (resolution - 1) * 2 - 1
-            py = y / (resolution - 1) * 2 - 1
-            
-            # Create start and end points for ray in camera space
-            start = Vector((px, py, -1))
-            end = Vector((px, py, 1))
-            
-            # Convert start and end points to world space
-            start_world = camera.matrix_world @ start
-            end_world = camera.matrix_world @ start + ray_length * (camera.matrix_world @ end - camera.matrix_world @ start).normalized()
-
-            # Append the vertices and edge
-            verts.append(start_world)
-            verts.append(end_world)
-            edges.append([len(verts)-2, len(verts)-1])
-
-    mesh.from_pydata(verts, edges, [])
+obj = bpy.context.active_object
+camera = bpy.data.objects["Camera"]
+cull_hidden_faces_by_camera(obj, camera)
 
 # Use this function after setting up your camera and object
 visualize_rays(bpy.data.objects["Camera"])
